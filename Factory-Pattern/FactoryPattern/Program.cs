a3acf462e3b0a4e3ba5c4c42668138eafad6cda8
AH-SHA <email.sa.coder@gmail.com>
2025-02-26 14:54:56 -0500

    Initial commit - GitHub Workflow issues
    Manually updating GitHub due to VSS and GitHub workflow issues.

---
+++ Factory-Pattern/FactoryPattern/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Diagnostics.Tracing;
+
+namespace FactoryPattern
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" ");
+            Console.WriteLine("Does the vehicle have 2 or 4 tires?");
+            var tires = int.Parse(Console.ReadLine());
+
+            if (tires != 2 && tires != 4)
+            {
+                Console.WriteLine("Try again, entered wrong number format or amount");
+            }
+
+            //Calling the GetVeichile Method, but need to use the static class infront, since its a static method.
+            //Creating a new variable to be able to call the GetVehicle Method and store the results of the scope of the method.
+
+            var answer = VehicleFactory.GetVehicle(tires);
+            Console.WriteLine();
+
+            //The newly created variable, can be used to access the individual methods existing in each class, which derived from the stubbed out method in the Interface.
+            answer.Drive();
+           
+            
+
+            
+
+        }
+    }
+}
---
+++ Factory-Pattern/FactoryPattern/VehicleFactory.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace FactoryPattern
+{
+    public static class VehicleFactory
+    {
+        
+        public static IVehicle GetVehicle(int tires)
+        {
+
+            switch (tires)
+            {
+                case 2:
+                    return new Motorcycle();
+                    break;
+
+                case 4:
+                    return new Car();
+                    break;
+
+                default:  //???Why can't the default term only use a break, instead of also needing a term
+                    return new Car();
+                    break;
+
+            }
+
+
+            
+            
+
+
+        }
+        
+     }
+
+        
+}
